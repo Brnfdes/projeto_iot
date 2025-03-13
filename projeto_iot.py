b6022f2bb9c912a653a5280710e9a84d1ffa8cf1
Bruna <100167564+Brnfdes@users.noreply.github.com>
2025-03-13 13:03:27 -0300

    Update projeto_iot.py

--- projeto_iot.py
+++ projeto_iot.py
@@ -1,44 +1,35 @@
 import machine
 import time
 import dht
-import conexao_wifi as wifi
-import urequests
+from conexao_wifi import conecta
 
 r = machine.Pin(2,machine.Pin.OUT)
-
-while True:
-	r.value(1)
-    time.sleep(1)
-	r.value(0)
-	time.sleep(1)
- 
-
-
 d = dht.DHT11(machine.Pin(4))
-while True:
-d.measure()
-print("Temperatura={}Umidade={}".format(d.temperature(),d.humidity()))
-time.sleep(3)
-
-if (d.temperature() > 31 or d.humidity() > 70) and r.value() == 0:
-      r.value(1)
-time.sleep(1)
-      if d.temperature() > 31:
-          print("A temperatura está acima de 31°C. Relé ligado")
-      else:
-          print("A umidade está acima de 70%. Relé ligado")
-
-def conecta(ssid, senha):
-    station = network.WLAN(network.STA_IF)
-    station.active(True)
-    station.connect(ssid, senha)
-    for t in range(50):
-        if station.isconnected():
-            break
-        time.sleep(0.1)
-    return station
 
 ssid = input("Rede: ")
 senha = input("Senha: ")
 print("Conectando...")
 station = conecta(ssid, senha)
+
+while True:
+    # Lê o sensor DHT11
+    d.measure()
+    print("Temperatura={} Umidade={}".format(d.temperature(), d.humidity()))
+    time.sleep(3)
+    
+    resposta = urequests.get("https://api.thingspeak.com/update?api_key=XGV138262JB85X8X&field1={}&field2={}".format(d.temperature(), d.humidity()))
+    
+    # Pisca o relé
+    r.value(1)
+    time.sleep(1)
+    r.value(0)
+    time.sleep(1)
+    
+    # Verifica temperatura e umidade
+    if (d.temperature() > 31 or d.humidity() > 70) and r.value() == 0:
+        r.value(1)
+        time.sleep(1)
+        if d.temperature() > 31:
+            print("A temperatura está acima de 31°C. Relé ligado")
+        else:
+            print("A umidade está acima de 70%. Relé ligado")
